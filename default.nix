1d8f292bc0d29863208d907fdceec1a30ae2037f
Anna <anna@hermes.ad.dk0.us>
2020-08-30 19:07:01 -0700

    Fixed typo

--- default.nix
+++ default.nix
@@ -1,6 +1,6 @@
 self: super: rec {
     libs = super.callPackage ./libs.nix { };
 
-    tilp = super.callPackage ./tilp/default.nix { inherit libs };
-    gfm = super.callPackage ./gfm/default.nix { inherit libs };
+    tilp = super.callPackage ./tilp/default.nix { inherit libs; };
+    gfm = super.callPackage ./gfm/default.nix { inherit libs; };
 }
