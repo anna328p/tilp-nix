101ddc8aa80763e752e54c928ef88634ff1ea63f
Anna <anna328p@gmail.com>
2020-08-30 20:15:27 -0700

    oops

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 self: super: let
     tilibs = super.callPackage ./libs.nix { };
 	args = with tilibs; { inherit libticalcs libticonv libtifiles libticables; }
-; in
+; in {
     tilp = super.callPackage ./tilp/default.nix args;
     gfm = super.callPackage ./gfm/default.nix args;
 }
